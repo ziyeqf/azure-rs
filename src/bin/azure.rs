1d3ddf0b9fc420b317c5dc5e2b1b594694dc2870
magodo <wztdyl@sina.com>
2025-09-15 21:12:18 +1000

    WASM support clap

--- src/bin/azure.rs
+++ src/bin/azure.rs
@@ -1,40 +1,27 @@
-use std::{path::PathBuf, str::FromStr};
-
 use anyhow::Result;
-use azure::api::ApiManager;
-use azure::arg::CliInput;
 use azure::client::Client;
-use azure::cmd;
+use azure::run;
 use azure_identity::DefaultAzureCredential;
+use std::{env, path::PathBuf, str::FromStr};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let api_manager = ApiManager::new(PathBuf::from_str("./metadata")?)?;
-
-    let matches = cmd::cmd().get_matches();
-    match matches.subcommand() {
-        Some(("api", matches)) => {
-            let args = if let Some(args) = matches.get_many::<String>("args") {
-                args.cloned().collect()
-            } else {
-                vec![]
-            };
-            let input = CliInput::new(args)?;
-            cmd::cmd_api(&api_manager, &input).get_matches();
-
-            // Invoke the api call
-            let ctx = api_manager.build_ctx(&input)?;
-            let credential = DefaultAzureCredential::new()?;
-            let client = Client::new(
-                "https://management.azure.com",
-                vec!["https://management.azure.com/.default"],
-                credential,
-                None,
-            )?;
-            let res = ctx.execute(&client).await?;
-            println!("{res}");
-            return Ok(());
-        }
-        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
-    }
+    let credential = DefaultAzureCredential::new()?;
+    let client = Client::new(
+        "https://management.azure.com",
+        vec!["https://management.azure.com/.default"],
+        credential,
+        None,
+    )?;
+    let res = run(
+        PathBuf::from_str("./metadata")?,
+        &client,
+        env::args_os()
+            .into_iter()
+            .map(|s| s.into_string().unwrap())
+            .collect(),
+    )
+    .await?;
+    println!("{res}");
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use anyhow::Result;
+use api::ApiManager;
+use arg::CliInput;
+use clap::{ArgMatches, Command};
+use client::Client;
+use std::path::PathBuf;
+
 pub mod api;
 pub mod arg;
 pub mod client;
@@ -5,3 +12,41 @@ pub mod cmd;
 
 #[cfg(target_arch = "wasm32")]
 pub mod wasm_exports;
+
+pub async fn run(p: PathBuf, client: &Client, raw_input: Vec<String>) -> Result<String> {
+    let api_manager = ApiManager::new(p)?;
+
+    let matches = get_matches(cmd::cmd(), raw_input.clone())?;
+
+    match matches.subcommand() {
+        Some(("api", matches)) => {
+            let args = if let Some(args) = matches.get_many::<String>("args") {
+                args.cloned().collect()
+            } else {
+                vec![]
+            };
+            let input = CliInput::new(args)?;
+            let _ = get_matches(cmd::cmd_api(&api_manager, &input), raw_input.clone())?;
+
+            // Invoke the api call
+            let ctx = api_manager.build_ctx(&input)?;
+            let res = ctx.execute(&client).await?;
+            return Ok(res);
+        }
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn get_matches(cmd: Command, input: Vec<String>) -> Result<ArgMatches> {
+    use anyhow::anyhow;
+    use clap::builder::Styles;
+    let cmd = cmd.styles(Styles::plain());
+    cmd.try_get_matches_from(input)
+        .map_err(|e| anyhow!("{}", e.render().ansi().to_string().replace("\\n", "\n")))
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn get_matches(cmd: Command, input: Vec<String>) -> Result<ArgMatches> {
+    Ok(cmd.get_matches_from(input))
+}
