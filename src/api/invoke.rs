59ba7d2c421b6398c47e860b80a950cc58de74a9
magodo <wztdyl@sina.com>
2025-09-16 15:59:49 +1000

    update

--- src/api/invoke.rs
+++ src/api/invoke.rs
@@ -1,141 +1,122 @@
-use std::{collections::HashMap};
+use core::unreachable;
+use std::collections::HashMap;
 
-// pub struct CommandParser {
-//     // pub metadata: super::metadata::Command,
-//     pub inner: clap::Command,
-// }
+use crate::client::Client;
 
-// impl CommandParser {
-//     pub fn parse<T, I>(self, args: I) -> clap::ArgMatches
-//     where
-//         I: IntoIterator<Item = T>,
-//         T: Into<std::ffi::OsString> + Clone,
-//     {
-//         let matches = self.inner.get_matches_from(args);
-//         return matches;
-//     }
-// }
-
-// impl From<super::metadata::Command> for CommandParser {
-//     fn from(metadata: super::metadata::Command) -> Self {
-//         let mut cmd = clap::Command::new(&metadata.name);
-//         for group in &metadata.arg_groups {
-//             let name = &group.name;
-//             for arg_meta in &group.args {
-//                 let mut arg = clap::Arg::new(&arg_meta.var).aliases(&arg_meta.options);
-//                 if let Some(help) = &arg_meta.help {
-//                     arg = arg.help(&help.short);
-//                 }
-//                 if let Some(true) = arg_meta.required {
-//                     arg = arg.required(true);
-//                 }
-//                 if let Some(group) = &arg_meta.group {
-//                     arg = arg.group(group);
-//                 }
-//                 cmd = cmd.arg(arg);
-//             }
-//         }
-//         Self { inner: cmd }
-//     }
-// }
-
-// pub struct CommandArg(HashMap<String, serde_json::Value>);
-
-// impl CommandArg {
-//     pub fn new() -> Self {
-//         Self(HashMap::new())
-//     }
-// }
+use super::metadata::{Command, Operation, Schema};
+use anyhow::{bail, Result};
+use clap::ArgMatches;
 
 pub struct CommandInvocation {
-    pub metadata: super::metadata::Command,
-    pub args: clap::ArgMatches,
+    command: Command,
+    matches: ArgMatches,
 }
 
 impl CommandInvocation {
-    pub async fn invoke(&self, client: &crate::client::Client) -> anyhow::Result<serde_json::Value> {
-        if self.metadata.operations.is_empty() {
-            anyhow::bail!("No operation found for command {}", self.metadata.name);
+    pub fn new(command: &Command, matches: &ArgMatches) -> Self {
+        Self {
+            command: command.clone(),
+            matches: matches.clone(),
         }
-        let operation = self.metadata.operations.first().unwrap();
-        let operation_ionvocation = OperationInvocation {
-            metadata: operation.clone(),
-            args: self.args.clone(),
-            ctx_args: HashMap::new(),
-        };
+    }
+
+    pub async fn invoke(&self, client: &Client) -> Result<serde_json::Value> {
+        if self.command.operations.is_empty() {
+            bail!("No operation found for command {}", self.command.name);
+        }
+        let operation = self.command.operations.first().unwrap();
+        let operation_ionvocation = OperationInvocation::new(operation, &self.matches);
         operation_ionvocation.invoke(client).await
     }
 }
 
-pub struct OperationInvocation {
-    pub metadata: super::metadata::Operation,
-    pub args: clap::ArgMatches,
-    pub ctx_args: HashMap<String, serde_json::Value>,
+struct OperationInvocation {
+    operation: Operation,
+    matches: ArgMatches,
 }
 
 impl OperationInvocation {
-    pub async fn invoke(&self, client: &crate::client::Client) -> anyhow::Result<serde_json::Value> {
-        if let Some(http) = &self.metadata.http {
-            let mut path = http.path.clone();
-            for param in &http.request.path.params {
-                if let Some(value) = self.args.get_one::<String>(&param.arg) {
-                    path = path.replace(&format!("{{{}}}", param.name), value);
-                } else if let Some(value) = self.ctx_args.get(&param.arg) {
-                    path = path.replace(&format!("{{{}}}", param.name), value.as_str().unwrap());
-                } else if let Some(true) = param.required {
-                    anyhow::bail!("Missing required path parameter: {}", param.name);
-                } else {
-                    panic!("Optional path parameter not supported yet!")
-                }
+    pub fn new(operation: &Operation, matches: &ArgMatches) -> Self {
+        Self {
+            operation: operation.clone(),
+            matches: matches.clone(),
+        }
+    }
+
+    pub async fn invoke(
+        &self,
+        client: &crate::client::Client,
+    ) -> anyhow::Result<serde_json::Value> {
+        if self.operation.http.is_none() {
+            anyhow::bail!(
+                r#"HTTP information not found for operation "{}""#,
+                self.operation
+                    .operation_id
+                    .clone()
+                    .unwrap_or("".to_string()),
+            );
+        }
+
+        let http = self.operation.http.as_ref().unwrap();
+        let mut path = http.path.clone();
+        for param in &http.request.path.params {
+            if let Some(value) = self.matches.get_one::<String>(&param.arg) {
+                path = path.replace(&format!("{{{}}}", param.name), value);
+            } else if let Some(true) = param.required {
+                anyhow::bail!("missing required path parameter: {}", param.name);
+            } else {
+                unreachable!(
+                    r#"optional path parameter "{}" not supported yet!"#,
+                    param.name
+                )
             }
-            let mut query_pairs = HashMap::new();
-            // TODO: handle query parameters
-            for param in &http.request.query.consts {
-                query_pairs.insert(param.name.clone(), param.default.value.clone());
-            }
-            let body: Option<bytes::Bytes> = if let Some(body_meta) = &http.request.body {
-                if let Some(schema) = &body_meta.json.schema {
-                    self.build_value(schema)?.map(|v| bytes::Bytes::from(v.to_string()))
-                } else {
-                    None
-                }
+        }
+        let mut query_pairs = HashMap::new();
+        // TODO: handle query parameters (query.params)
+        for param in &http.request.query.consts {
+            query_pairs.insert(param.name.clone(), param.default.value.clone());
+        }
+        let body: Option<bytes::Bytes> = if let Some(body_meta) = &http.request.body {
+            if let Some(schema) = &body_meta.json.schema {
+                self.build_value(schema.clone())?
+                    .map(|v| bytes::Bytes::from(v.to_string()))
             } else {
                 None
-            };
-            let response = client.run(http.request.method.into(), path.as_str(), &query_pairs["api-version"], body, None).await?;
-            for response_meta in &http.responses {
-                if let Some(status_codes) = &response_meta.status_code {
-                    if status_codes.contains(&(u16::from(response.status_code) as i64)) {
-                        // if let Some(json_meta) = response_meta.body.as_ref().map(|body| &body.json) {
-                        //     if let Some(schema) = &json_meta.schema {
-                        //         let v: serde_json::Value = serde_json::from_slice(&response.body)?;
-                        //         println!("{}", serde_json::to_string_pretty(&v)?);
-                        //         return Ok(());
-                        //     }
-                        // }
-                        return Ok(serde_json::from_slice(&response.body)?);
-                    }
-                } else if let Some(true) = response_meta.is_error {
-                    anyhow::bail!("Error response: {}", String::from_utf8_lossy(&response.body));
+            }
+        } else {
+            None
+        };
+        let response = client
+            .run(
+                http.request.method.into(),
+                path.as_str(),
+                &query_pairs["api-version"],
+                body,
+                None,
+            )
+            .await?;
+        for response_meta in &http.responses {
+            if let Some(status_codes) = &response_meta.status_code {
+                if status_codes.contains(&(u16::from(response.status_code) as i64)) {
+                    return Ok(serde_json::from_slice(&response.body)?);
                 }
             }
         }
-        anyhow::bail!("No HTTP information found for this operation");
+        anyhow::bail!(
+            "error response: {}\n\n{}",
+            response.status_code,
+            String::from_utf8_lossy(&response.body)
+        );
     }
 
-    fn build_value(
-        &self,
-        schema: &super::metadata::Schema,
-    ) -> anyhow::Result<Option<serde_json::Value>> {
+    fn build_value(&self, schema: Schema) -> Result<Option<serde_json::Value>> {
         match schema.type_.as_str() {
             "object" => {
                 if let Some(arg) = &schema.arg {
-                    if let Some(value) = self.ctx_args.get(arg) {
-                        Ok(Some(value.clone()))
-                    } else if let Some(value) = self.args.get_one::<String>(arg) {
+                    if let Some(value) = self.matches.get_one::<String>(arg) {
                         Ok(Some(serde_json::from_str(value)?))
                     } else if let Some(true) = schema.required {
-                        anyhow::bail!("Missing required object property: {}", arg);
+                        anyhow::bail!("missing required object property: {}", arg);
                     } else {
                         let mut map = serde_json::Map::new();
                         if let Some(props) = &schema.props {
@@ -158,9 +139,9 @@ impl OperationInvocation {
             }
             s if s.starts_with("array") => {
                 if let Some(arg) = &schema.arg {
-                    if let Some(value) = self.ctx_args.get(arg) {
+                    if let Some(value) = self.args.get(arg) {
                         Ok(Some(value.clone()))
-                    } else if let Some(value) = self.args.get_one::<String>(arg) {
+                    } else if let Some(value) = self.matches.get_one::<String>(arg) {
                         Ok(Some(serde_json::from_str(value)?))
                     } else if let Some(true) = schema.required {
                         anyhow::bail!("Missing required array property: {}", arg);
@@ -173,9 +154,9 @@ impl OperationInvocation {
             }
             "string" => {
                 if let Some(arg) = &schema.arg {
-                    if let Some(value) = self.ctx_args.get(arg) {
+                    if let Some(value) = self.args.get(arg) {
                         Ok(Some(value.clone()))
-                    } else if let Some(value) = self.args.get_one::<String>(arg) {
+                    } else if let Some(value) = self.matches.get_one::<String>(arg) {
                         Ok(Some(serde_json::Value::String(value.clone())))
                     } else if let Some(true) = schema.required {
                         anyhow::bail!("Missing required string property: {}", arg);
@@ -188,9 +169,9 @@ impl OperationInvocation {
             }
             s if s.starts_with("integer") => {
                 if let Some(arg) = &schema.arg {
-                    if let Some(value) = self.ctx_args.get(arg) {
+                    if let Some(value) = self.args.get(arg) {
                         Ok(Some(value.clone()))
-                    } else if let Some(value) = self.args.get_one::<i32>(arg) {
+                    } else if let Some(value) = self.matches.get_one::<i32>(arg) {
                         Ok(Some((*value).into()))
                     } else if let Some(true) = schema.required {
                         anyhow::bail!("Missing required integer property: {}", arg);
@@ -203,9 +184,9 @@ impl OperationInvocation {
             }
             "boolean" => {
                 if let Some(arg) = &schema.arg {
-                    if let Some(value) = self.ctx_args.get(arg) {
+                    if let Some(value) = self.args.get(arg) {
                         Ok(Some(value.clone()))
-                    } else if let Some(value) = self.args.get_one::<bool>(arg) {
+                    } else if let Some(value) = self.matches.get_one::<bool>(arg) {
                         Ok(Some((*value).into()))
                     } else if let Some(true) = schema.required {
                         anyhow::bail!("Missing required boolean property: {}", arg);
@@ -219,9 +200,9 @@ impl OperationInvocation {
             _ => {
                 // We suppose any other type as a json value first, if failed, try to parse it as a string
                 if let Some(arg) = &schema.arg {
-                    if let Some(value) = self.ctx_args.get(arg) {
+                    if let Some(value) = self.args.get(arg) {
                         Ok(Some(value.clone()))
-                    } else if let Some(value) = self.args.get_one::<String>(arg) {
+                    } else if let Some(value) = self.matches.get_one::<String>(arg) {
                         match serde_json::from_str(value) {
                             Ok(v) => Ok(Some(v)),
                             Err(_) => Ok(Some(serde_json::Value::String(value.clone()))),
